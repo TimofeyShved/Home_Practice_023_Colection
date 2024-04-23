113b111499f09fd64cb093eadb33320cee832944
TimofeyShved <timofeya123@inbox.ru>
2024-04-23 13:26:26 +0600

    practic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,18 @@
 package com.company;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Map<String, Integer> s = new HashMap<String, Integer>();
-        s.put("1", 1);
-        s.put("sss", 1);
-        s.put("2", 2);
-        s.put("sss", 2);
-        s.put("3", 4);
-        s.put("3", 2);
-        s.put("s", 3);
+        s.put("1 000", 1);
+        s.put("2 500", 2);
+        s.put("Squad", 2);
+        s.put("3 000", 4);
+        s.put("Olimp", 3);
+        s.put("SSS+", 3);
+        s.put("Zorro", 2);
 
         Map<Integer, Collection<String>> map = inversToMap(s);
 
@@ -27,7 +25,17 @@ public class Main {
         Map<V, Collection<K>> newMap = new HashMap<V, Collection<K>>();
 
         for (K k: map.keySet()){
+            newMap.put(map.get(k), null);
+        }
 
+        for (V v: newMap.keySet()){
+            Collection<K> collection = new ArrayList<K>();
+            for (K k: map.keySet()){
+                if(v.equals(map.get(k))){
+                    collection.add(k);
+                }
+            }
+            newMap.put(v, collection);
         }
 
         return newMap;
